952c00e08cc41080ce6d45e630907126908a3310
davisweredev <davisweredev@gmail.com>
2025-11-26 13:43:53 +0300

    update notification model

--- notification/models.py
+++ notification/models.py
@@ -5,19 +5,23 @@ class Notification(models.Model):
     message = models.TextField()
     is_read = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
-    reciever = models.ForeignKey(
-        User, 
-        on_delete=models.CASCADE, 
-        related_name="notifications"
-    )
     sender = models.ForeignKey(
         User, 
-        null=True, 
-        blank=True, 
-        on_delete=models.SET_NULL, 
-        related_name="sent_notifications"
+        on_delete=models.CASCADE, 
+        related_name='sent_notifications'
     )
+    receiver = models.ForeignKey(
+        User, 
+        on_delete=models.CASCADE, 
+        related_name='received_notifications'
+    )  
 
+    class Meta:
+        ordering = ['created_at']
 
     def __str__(self):
-        return f"Notification to {self.user.username}: {self.message[:20]}"
+        sender_name = self.sender.username if self.sender else "System"
+        return f"Notification to {self.receiver.username} \
+            from {sender_name}: {self.message[:20]}"
+        
+
